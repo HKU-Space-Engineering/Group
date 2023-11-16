ee2cfe0fd19b6189314a9a0e8ed80eef1cc9988d
JCHB00 <79236706+JCHB00@users.noreply.github.com>
2023-11-16 15:24:33 +0800

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,25 @@
+/**@author Jian Chun Hin 20207461 */
+/*Date_Create:16/11/2023 */
+/*It is a class for Login */
+
+
+public class Login {
+//Field
+    String uName;
+    int uID;
+    int uBirth;
+    String uSchool;
+//Constructor
+    public Login(String Name,int ID,int Birth,String School){
+        // Constructor for the class -> Login.java 
+        uName = Name;
+        uID = ID;
+        uBirth = Brith;
+        uSchool = school;
+    }
+//Method
+    public void CreateAccount(){
+        //出現資料填寫頁
+        
+    }
+}
