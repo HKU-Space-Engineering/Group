68a38b31d8d81b564df8b05f1f326243487cf09c
JCHB00 <79236706+JCHB00@users.noreply.github.com>
2023-11-16 15:23:24 +0800

    Main - Learning Buddy
    It is a main programming of the learning Buddy

---
+++ Learning Buddy.java
@@ -0,0 +1,26 @@
+/**Learning Buddy
+ * @author Jian Chun Hin 
+ * 
+ */
+public class Learning Buddy {
+//field
+String uName;
+int uID;
+int uBirth;
+String uSchool;
+
+    public void main(String[] args){
+        System.out.printf("Welcome to the Learning Buddy");
+        try{
+            //Check does the user has created their account yet.
+            //if not, then run "Login.CreateAccount()" in catch(),else keep running
+        }catch();
+
+
+    }
+
+    public void Exception1(){
+        System.out.printf("Please Input your data!\n");//這裏要變成interface , 原因是 print與GUI 不相容。
+        Login.CreateAccount()
+    }
+}
