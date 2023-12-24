02ac5e8b3009ca889e3896705a26d0d55b1589de
Heba0405 <117824100+Heba0405@users.noreply.github.com>
2023-12-25 00:09:45 +0800

    Add files via upload

---
+++ Achievement.java
@@ -0,0 +1,14 @@
+/* *@author Lam Tsun Ting 20219894 */
+/*date created 24/12/2023 */
+
+public class Achievement {
+    //method
+    public static void GetAch() {
+        //if they unlock our achievements, they can get them on their achievement list
+
+    }
+    public static void PrintAch() {
+        //convert user's achievement list into txt file
+        //users can save it on desktop or print it out
+    }
+}
