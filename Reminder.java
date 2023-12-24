75e026cf9037be417fc5bdb7d5e04469f5476980
joeis1201 <148559035+joeis1201@users.noreply.github.com>
2023-12-24 18:25:01 +0800

    Add files via upload

---
+++ Reminder.java
@@ -0,0 +1,15 @@
+/* *@author Chau Siu Hong 20186650 */
+/*date created 24/12/2023 */
+
+public class Reminder {
+//method
+    public static void CreateBookmark() {
+    //need GUI for users to input what to study,expected time to finish 
+    //also can choose style/design of the bookmark(how to get the different design and apply it??)
+    //and button to create it -> write into a txt file
+    }
+    public static void CheckBookmark() {
+    //button to check the created bookmarks(read the file) -> a frame show the list 
+    //the list jump out when run the program -> maybe connect to the login.java
+    } 
+}
