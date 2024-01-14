15d26b01f3beec6453a616a680b5084d9e22a092
JCHB00 <79236706+JCHB00@users.noreply.github.com>
2024-01-14 12:23:59 +0800

    A python program about get the wikiepdia
    You can find the py file and exe file.

---
+++ Wiki_2.py
@@ -0,0 +1,25 @@
+# Author: Jian Chun Hin, Ben , 20207461
+#This python program need to download a new module, which is wikipedia-api
+# Download Method:
+#1.open cmd
+#write: pip3 install wikipedia-api
+
+import wikipediaapi
+
+Rearch = "Kate_Sheppard"#預設Kate_Sheppard 為目標
+
+with open("Rearchfile.txt","r") as rr:
+    Rearch = rr.readline()
+
+#setting
+wiki_wiki = wikipediaapi.Wikipedia("WikiSearcher (1704935142jian@gmail.com)",'en',extract_format = wikipediaapi.ExtractFormat.WIKI)
+#start
+p_wiki = wiki_wiki.page(Rearch)
+dStr = p_wiki.text
+print(dStr)
+
+
+with open("Data.txt","w+",encoding="utf-8") as download:
+    #將data寫入Data.text
+    download.write(dStr)
+    
