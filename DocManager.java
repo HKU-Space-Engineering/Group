f26cddd2f66c96b5846b5273c9e9c6c0e849b66a
EdgeChan <56496024+EdgeChan@users.noreply.github.com>
2024-01-16 21:58:23 +0800

    Add files via upload

---
+++ DocManager.java
@@ -0,0 +1,18 @@
+/* *@author Chan Tze Hei Dawn 20219923 */
+/*date created 24/12/2023 */
+
+public class DocManager {
+    //method
+        public static void NotesImport() {
+        //Import selected files from files explorer to Learning buddy's library.
+        }
+        public static void FolderGroup() {
+        // Add selected notes into folder, create a new folder if needed.
+        } 
+        public static void Folderedit() {
+        //Changing Folder's Name, Icon and colour.
+        }
+        public static void Folderconvert() {
+            //File format converter.
+            }
+    }
