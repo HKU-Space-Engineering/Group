8cd963802b92de9d84753e4e007e31687176b8a9
leowannasleep <62427261+leowannasleep@users.noreply.github.com>
2024-01-20 21:30:23 +0800

    Update DocManager.java

--- DocManager.java
+++ DocManager.java
@@ -3,6 +3,30 @@
 
 public class DocManager {
     //method
+        public static void DocManager(){
+        try{
+            JFrame frame = new JFrame();
+            frame.setSize(500,500);
+            frame.setLocation(400,400);
+            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);// It is necessary
+        
+            /*Create a Panel*/
+            JPanel panel = new JPanel();//It create a Panel
+            frame.add(panel); // Add the panel
+            ReminderP(panel); // Design the Panel + use the method
+            frame.setVisible(true);
+        }catch(Exception e){}
+    }
+
+    public static void DocManagerP(JPanel panel){
+        panel.setLayout(null);
+
+        JLabel ReminderLabel = new JLabel("DocManager");// Setting the position of Panel
+        ReminderLabel.setBounds(10,20,80,40);  // setBounds(x,y,width,height);
+        panel.add(DocManagerLabel); //add the label into the Panel
+
+        
+    }
         public static void NotesImport() {
         //Import selected files from files explorer to Learning buddy's library.
         }
