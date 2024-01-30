08861f4cdf788c7e488e456bd2d4c515f993c693
joeis1201 <148559035+joeis1201@users.noreply.github.com>
2024-01-30 12:24:42 +0800

    30/1/2024 update

--- Reminder.java
+++ Reminder.java
@@ -27,21 +27,51 @@ public class Reminder {
         panel.setLayout(null);
 
         JLabel ReminderLabel = new JLabel("Reminder");// Setting the position of Panel
-        ReminderLabel.setBounds(10,20,80,40);  // setBounds(x,y,width,height);
+        ReminderLabel.setBounds(120,20,80,40);  // setBounds(x,y,width,height);
         panel.add(ReminderLabel); //add the label into the Panel
 
-        JLabel bookmarkLabel = new JLabel("Please input your study target");
-        bookmarkLabel.setBounds(10,40,200,25);
+        //create bookmark
+        //label
+        JLabel bookmarkLabel = new JLabel("Please input your study plan");
+        bookmarkLabel.setBounds(120,50,200,25);
         panel.add(bookmarkLabel);
 
         JLabel contentLabel = new JLabel("Content:");
-        contentLabel.setBounds(10,60,100,25);
+        contentLabel.setBounds(120,80,100,25);
         panel.add(contentLabel);
 
-        //textbox
+        JLabel exptimeLabel = new JLabel("Expected time:");
+        exptimeLabel.setBounds(120,110,100,25);
+        panel.add(exptimeLabel);
+
+        JLabel slash = new JLabel("/");
+        slash.setBounds(250,110,30,25);
+        panel.add(slash);
+
+        //textbox to input
         JTextField content = new JTextField(20);
-        content.setBounds(80,60,100,25);
+        content.setBounds(190,80,100,25);
         panel.add(content);
+
+        JTextField expmonth = new JTextField(20);
+        expmonth.setBounds(210,110,30,25);
+        panel.add(expmonth);
+
+        JTextField expdate = new JTextField(20);
+        expdate.setBounds(260,110,30,25);
+        panel.add(expdate);
+
+        //button to create
+        JButton Create = new JButton("Create");
+        Create.setBounds(170,170,100,25);
+        Create.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event){
+                String text = content.getText() + expmonth.getText() + expdate.getText();
+                Storage.TextFileInput("reminder.txt", text);
+                JOptionPane.showMessageDialog(null,"You create a bookmark Successfully!");
+            }
+        });
+        panel.add(Create);
     }
 
     public static void CreateBookmark() {
@@ -53,8 +83,22 @@ public class Reminder {
     public static void CheckBookmark() {
     //button to check the created bookmarks(read the file) -> a frame show the list 
     //the list jump out when run the program -> maybe connect to the login.java
-    } 
+        try{
+            BufferedReader bufferReader = new BufferedReader(new FileReader("reminder.txt"));
+            ArrayList<String> AList = new ArrayList<String>(); // arraylist
+            // while loop to read file as string lines, until end (null string)
+            String strLine;
+            while ((strLine = bufferReader.readLine()) != null) { // read a line
+                AList.add(strLine); // add read string line data to arraylist
+            }
+            bufferReader.close(); //close the stream
+            // BELOW: Convert ArrayList to String array
+            String[] bookmarks = AList.toArray(new String[retAList.size()]);
+        } catch (IOException ioE){
 
+        }
+
+    }
     public static void main(String[] args){
         Reminder();
     }
