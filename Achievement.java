3381ff2e4bc85c47f609e1c327ae624b59877733
Heba0405 <117824100+Heba0405@users.noreply.github.com>
2024-01-31 15:49:41 +0800

    Achievement.java

--- Achievement.java
+++ Achievement.java
@@ -8,32 +8,30 @@ import java.util.*;
 
 public class Achievement {
 
-    public static void Achievement(){
+    public static void achievement(){
         try{
             JFrame frame = new JFrame();
-            frame.setSize(500,500);
-            frame.setLocation(400,400);
+            frame.setSize(700,700);
+            frame.setLocation(640,200);
             frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);// It is necessary
         
             /*Create a Panel*/
             JPanel panel = new JPanel();//It create a Panel
             frame.add(panel); // Add the panel
-            AchievementP(panel); // Disgin the Panel + use the method
+            achievementP(panel); // Design the Panel + use the method
             frame.setVisible(true);
 
         }catch(Exception e){}
     }
 
-    public static void AchievementP(JPanel panel){
+    public static void achievementP(JPanel panel){
 
         panel.setLayout(null);
         
         JLabel AchievementLabel = new JLabel("Achievement");// Seting the position of Panel
-        AchievementLabel.setBounds(10,20,80,40);  // setBounds(x,y,width,height);
+        AchievementLabel.setBounds(305,25,80,40);  // setBounds(x,y,width,height);
         panel.add(AchievementLabel); //add the label into the Panel
     }
-}
-
 
 public static void GetAch() {
     //if they unlock our achievements, they can get them on their achievement list
