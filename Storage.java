cf354cd0eb42e0a7ae8abb9ba0a98c60fdfaedbb
JCHB00 <79236706+JCHB00@users.noreply.github.com>
2024-03-18 13:08:19 +0800

    2024/3/18

--- Storage.java
+++ Storage.java
@@ -1,6 +1,8 @@
 /**@author Jian Chun Hin,20207461,Ben */
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
+import javax.swing.JOptionPane;
 
 /*This class aims to provide a FileIO function. Such as storage some text in a text file */
 public class Storage {
@@ -20,5 +22,22 @@ public class Storage {
         File a = new File(FName);
         a.delete();
         System.out.println("Delete File Successfully");
+    }
+
+    public static String TextFileRead(String FName){
+        String output = "";
+        try{
+            String data; //it is the var to store the data from the txt file
+            BufferedReader bufferReader = new BufferedReader(new FileReader(FName,StandardCharsets.UTF_8));
+            while ((( data = bufferReader.readLine()) != null)){
+                if(data == " "){
+                    break;
+                }else{
+                    output = data;
+                }
+            }
+            bufferReader.close();
+        }catch(IOException e){}
+        return output;
     }
 }
